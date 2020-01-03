2594448e88606987d08eba8409c6f89d960d5f6f
j123qwe <42499831+j123qwe@users.noreply.github.com>
2020-01-03 13:35:43 -0600

    Update deploy.sh
    Added docker-compose package

--- deploy.sh
+++ deploy.sh
@@ -41,7 +41,7 @@ install_docker(){
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - #Download Docker GPG key
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" #Add Docker repository
 	sudo apt update #Refresh APT
-	sudo apt install -y docker-ce docker-ce-cli containerd.io #Install Docker
+	sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose #Install Docker
 }
 
 install_webmin(){
